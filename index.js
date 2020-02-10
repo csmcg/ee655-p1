a0defb1ecaf699f45d820581bff9a078c11c846c
Connor McGarty <cmcgarty@uab.edu>
2020-02-10 02:10:03 -0600

    worked on adding and updating

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const _ = require('lodash');
 
+var BreakException = {};
 const port = 3000;
+app.use(bodyParser.urlencoded({extended: false}));
+
+/* the API should implement the following commands:
+ *		- /products - returns a list of the attributes of all prods
+ *		- /products/id - returns the attributes of the prod. whose id is spec'd
+ *		- /delete/id - removes the product w/ the spec'd id from inv
+ *		- /delete - removes all products from the inventory
+ *		- /add_product/id - adds a new product w/ spec'd id into inventory
+ *		- /update_product/id
+*/
 
 let products = [{id: 0,
 				 name: "apple",
@@ -13,6 +25,10 @@ let products = [{id: 0,
 				 quantity: 10,
 				 price: 5.99},
 				{id: 2,
+				 name: "test",
+				 quantity: 2,
+				 price: 3.00},
+				{id: 3,
 				 name: "banana",
 				 quantity: 12,
 				 price: 8.99}];
@@ -39,17 +55,69 @@ app.get('/products/:id', (req, res) => {
 	res.end(JSON.stringify(products[req.params.id]));
 });
 
-/* app.get('/products/delete/:id', (req, res) => {
+app.get('/delete/:id', (req, res) => {
 	console.log('Product ID:' + req.params.id + ' requested for deletion.');
 	// delete the product
-	
-}); */
+	products.forEach(product => {
+		if (product.id == req.params.id) {
+			_.remove(products, product);
+		}
+	});
+	sortProducts(products);
+	console.log(products);
+});
 
-/* app.post('/add_product', (req, res) => {
-	
-}); */
+app.post('/add_product/id', (req, res) => {
+	new_product = {};
+	new_product.id = Number(req.body.id);
+	new_product.name = req.body.name;
+	new_product.quantity = Number(req.body.quantity);
+	new_product.price = Number(req.body.price);
+	console.log(`New product added: ${JSON.stringify(new_product)}`);
+	products.push(new_product);
+	sortProducts(products);
+	console.log('Products state: ' + products);
+	res.sendFile(__dirname + '/productadded.html');
+});
+
+app.post('/update_product/id', (req, res) => {
+	updated_product = {};
+	try { products.forEach((product) => {
+		if (req.body.id == product.id) {
+			Object.keys(product).forEach((key) => {
+				if (product[key] == "") {
+					; // don't update this field
+					updated_product[key] = product[key];
+				}
+				else {
+					product[key] = req.body.key;
+					updated_product[key] = product[key];
+				};
+			});
+			// product updated
+			console.log('Updated product: ' + JSON.stringify(updated_product));
+			sortProducts(products);
+			throw BreakException;
+		}
+	});
+	} catch (e) {
+		if (e !== BreakException) throw e;
+		else {
+			res.sendFile(__dirname + '/updatedproduct.html');
+		}
+	};
+	res.end('<html><head><title>Product not found</title></head><body>Product was not found under that ID. Click <a href=\"http://localhost:3000/\">here</a> to return home and try again.</body></html>"');
+});
 
 app.listen(port, () => {
 	console.log(`server listening on port ${port}`);
 })
 
+function sortProducts(array) {
+	array.sort((a, b) => {
+		return a.id < b.id ? -1
+			 : b.id < a.id ?  1
+			 : 0;
+	});
+	return array;
+}
